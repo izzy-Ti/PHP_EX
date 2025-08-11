707ddfd0d2bf1be1fd60d775a36cda345f21cfcc
izzy-Ti <israelashenafi29@gmail.com>
2025-08-11 07:03:33 -0700

    arrays and associative arrays

--- index.php
+++ index.php
@@ -1,33 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action = "index.php" method = "post">
-        <label>x</label><br>
-        <input type="text" name= "x"><br>
-        <label>y</label><br>
-        <input type="text" name= "y"><br>
-        <input type="submit" value= "calculate"><br>
-    </form >
-    <?php
-        $y = $_POST["y"];
-        $x = $_POST["x"];
-        $number = null;
-        $number = abs($x);
-        $number = floor($x);
-        $number = round($x);
-        $number = ceil($x);
-        $number = pow($x, $y);
-        $number = max($x, $y);
-        $number = min($x, $y);
-        $number = pi();
-        $number = rand(1, 20);
-        echo $number  
+<?php
+    $name = array('abebe', 'zeleke', 'nahom', 'israel');
+    array_push($name, "alemitu", "tina");
+    array_pop($name);
+    foreach($name as $food){
+        echo $food . "<br>";
+    };
 
-    ?>
-</body>
-</html>
+    $comp = array(
+        "ZEMS" => "3emp",
+        "APL" => "45emp",
+        "ERT" => "32emp"
+    );
+    echo $comp["ERT"] . "<br>";
+
+    $comp["ZEMS"] = "2133";
+    array_pop($comp);
+
+    foreach($comp as $key => $value){
+        echo $key . "=>" . $value . "<br>";
+    }
+?>
